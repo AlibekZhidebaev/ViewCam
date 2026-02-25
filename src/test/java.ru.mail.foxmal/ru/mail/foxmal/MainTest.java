fe12eb5198ba8797d9f9ba9f411676cb32585461
User <foxmal81@gmail.com>
2026-02-25 10:47:15 +0500

    create_project_and_maven_settings

---
+++ src/test/java.ru.mail.foxmal/ru/mail/foxmal/MainTest.java
@@ -0,0 +1,12 @@
+package ru.mail.foxmal;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void mainShouldRunWithoutErrors() {
+        Main.main(new String[]{});
+    }
+}
